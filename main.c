af9d324a30d19216c88f3df2f72853b96de10b8c
Juan Thales <juanthalesap@gmail.com>
2025-12-02 21:39:42 -0300

    primeiro hello world em C!!!

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // lib de entrada e saida de dados do C
+#include <stdlib.h> // usar isso no windows para nao fechar o programa apos rodar 
+
+// funcao principal 
+int main() {
+  printf("Hello, World! \n ");
+
+  system("pause"); // usar isso no windows para nao fechar o programa apos rodar 
+  return 0; // diz que o programa rodou com sucesso
+}
