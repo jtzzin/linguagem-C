867a91ab19c4483c69a7ac90243b6eecaeb173fa
Juan Thales <juanthalesap@gmail.com>
2025-12-03 01:20:25 -0300

    printando mais de uma variavel em sequencia e com espaco

--- main.c
+++ main.c
@@ -3,7 +3,17 @@
 
 // funcao principal 
 int main() {
-  printf("Hello, World! \n ");
+  //printf("Hello, World! \n ");
+
+  int n1 = 20;
+  int n2 = 10;
+  int n3 = 5;
+
+  printf(" --------------------- ---------------- \n ");
+  printf("o valor do numero um é: %d \n----------------- \n", n1), 
+  printf("o valor do numero dois é: %d \n----------------- \n", n2),
+  printf("o valor do numero tres é: %d \n----------------- \n", n3);
+
 
   system("pause"); // usar isso no windows para nao fechar o programa apos rodar 
   return 0; // diz que o programa rodou com sucesso
