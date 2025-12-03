9080b695f4cf0f4861050fb8c5e0b6d1c5f79130
Juan Thales <juanthalesap@gmail.com>
2025-12-03 03:35:45 -0300

    boletim simples

--- main.c
+++ main.c
@@ -5,14 +5,42 @@
 int main() {
   //printf("Hello, World! \n ");
 
-  int n1 = 20;
+  /*int n1 = 20;
   int n2 = 10;
   int n3 = 5;
+
+  char time[] = "coringao"; // devo criar sempre um array de character para escrever "string" em C
+  float torcida = 35.00;
 
   printf(" --------------------- ---------------- \n ");
   printf("o valor do numero um é: %d \n----------------- \n", n1), 
   printf("o valor do numero dois é: %d \n----------------- \n", n2),
   printf("o valor do numero tres é: %d \n----------------- \n", n3);
+  printf("o nome do meu time é: %s  \n-------------------\n", time);
+  printf("e temos mais de %2.f milhoes de torcedores! \n", torcida); // %2.f (2.f - duas casas decimais)
+  printf("-------------------------- \n");
+  */
+
+// boletim ficticio de um aluno escolar
+  printf(" -------- Bem-vindo ao gerenciador de boletins escolares! ---------\n");
+
+  int numero_aluno;
+  float nota;
+  char situacao;
+// esse "&" antes da varial significa -> endereco de memoria
+  printf("Informe a situacao final do aluno: \n");
+  scanf("%c", &situacao);
+
+  printf("Digite o numero do aluno: \n");
+  scanf("%d", &numero_aluno);
+
+  printf("Digite a nota do aluno: \n");
+  scanf("%f", &nota);
+  printf("-----------");
+  
+
+ 
+
 
 
   system("pause"); // usar isso no windows para nao fechar o programa apos rodar 
